4e8501013b3b9f0ac7e7943c5c58d127781f683d
caleb <yocalebo@gmail.com>
2020-12-18 10:19:26 -0500

    fix setup.py by adding name, version, and setup_requires

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup
 from Cython.Build import cythonize
 
 setup(
+    name='libsgio',
+    version='0.0.1',
+    setup_requires=[
+        'setuptools>=45.0',
+        'Cython',
+    ],
     ext_modules = cythonize('libsgio.pyx')
 )
